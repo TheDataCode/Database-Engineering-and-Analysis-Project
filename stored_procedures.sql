ca31bbe5b7c32f18f82a4ed28463718df8c1e3cb
Zainab Mohammed <zm16043@gmail.com>
2023-05-11 11:58:53 +0000

    select max quantity

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -3,14 +3,5 @@ Create Procedure GetMaxQuantity() select max(quantity) from itemorder;
 CALL GetMaxQuantity();
 
     
-# Create a Procedure that cancels an order based on user input of an order_id
-DELIMITER //
-CREATE PROCEDURE CancelOrder (IN orderid INT) 
-BEGIN
-DELETE FROM itemorder where orderID = orderid;
-SELECT CONCAT('orderID ',orderid,' has been cancelled');
-END //
-DELIMITER ;
-
 
 
