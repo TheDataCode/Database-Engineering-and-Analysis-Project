9ce7e3b17a105f1bbd9504dabfaabf53f482e08c
Zainab Mohammed <zm16043@gmail.com>
2023-05-11 11:59:55 +0000

    Cancel order

--- storedProcedure-Delete_statement.sql
+++ storedProcedure-Delete_statement.sql
@@ -7,3 +7,14 @@ BEGIN
 END //
 DELIMITER ;
 
+
+
+# Create a Procedure that cancels an order based on user input of an order_id
+DELIMITER //
+CREATE PROCEDURE CancelOrder (IN orderid INT) 
+BEGIN
+DELETE FROM itemorder where orderID = orderid;
+SELECT CONCAT('orderID ',orderid,' has been cancelled');
+END //
+DELIMITER ;
+
